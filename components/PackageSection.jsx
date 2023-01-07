9a020c7460916dc82465d48b9b59452277033b92
shahariar50 <shahariar.touhid@gmail.com>
2023-01-07 23:54:47 +0600

    setup package section

--- components/PackageSection.jsx
+++ components/PackageSection.jsx
@@ -1,5 +1,121 @@
+/* eslint-disable react/no-unescaped-entities */
 const PackageSection = () => {
-  return <div>PackageSection</div>;
+  return (
+    <section className="py-8" id="pricing">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8 col-lg-6 col-xxl-5 text-center mb-3">
+            <h6 className="fw-bold fs-4 display-3 lh-sm mb-3">
+              Get awesome features, without extra charges
+            </h6>
+            <p className="mb-4">
+              The rise of mobile devices transforms the way we consume
+              information entirely and the world's most elevant channels such as
+              Facebook.
+            </p>
+          </div>
+        </div>
+        <div className="row flex-center">
+          <div className="col-12 mb-3">
+            <div className="d-flex justify-content-center">
+              <label className="form-check-label me-2" for="customSwitch1">
+                Monthly
+              </label>
+              <div className="form-check form-switch">
+                <input
+                  className="form-check-input"
+                  id="customSwitch1"
+                  type="checkbox"
+                  checked="checked"
+                />
+                <label
+                  className="form-check-label align-top"
+                  for="customSwitch1"
+                >
+                  Yearly
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="card shadow-lg mb-4 border-0">
+              <div className="card-header border-bottom-0 pt-7 pb-5">
+                <div className="d-flex justify-content-center">
+                  <h1 className="fw-bold">$0</h1>
+                  <span className="d-flex align-items-center">/month</span>
+                </div>
+                <h5 className="fw-bold text-center">Business className</h5>
+                <span className="text-700 text-center d-block">
+                  For small teams or office
+                </span>
+              </div>
+              <div className="card-body mx-auto">
+                <ul className="list-unstyled mb-4">
+                  <li className="text-700 py-2 text-secondary">
+                    Darg &amp; Drop Builder
+                  </li>
+                  <li className="text-700 py-2 text-secondary">
+                    1,000's of Templates
+                  </li>
+                  <li className="text-700 py-2 text-secondary">
+                    Blog Support Tools
+                  </li>
+                  <li className="text-700 py-2 text-secondary">
+                    eCommerce Store
+                  </li>
+                </ul>
+                <a
+                  className="btn btn-lg btn-primary rounded-pill mb-3"
+                  href="#"
+                >
+                  Start free trial
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="card shadow-lg mb-4">
+              <div className="card-header border-bottom-0 pt-7 pb-5">
+                <div className="d-flex justify-content-center">
+                  <h1 className="fw-bold">$99</h1>
+                  <span className="d-flex align-items-center">/month</span>
+                </div>
+                <h5 className="fw-bold text-center">Pro Master</h5>
+                <span className="text-700 text-center d-block">
+                  For small teams or office
+                </span>
+              </div>
+              <div className="card-body mx-auto">
+                <ul className="list-unstyled mb-4">
+                  <li className="text-700 py-2 text-secondary">
+                    Darg &amp; Drop Builder
+                  </li>
+                  <li className="text-700 py-2 text-secondary">
+                    1,000's of Templates
+                  </li>
+                  <li className="text-700 py-2 text-secondary">
+                    Blog Support Tools
+                  </li>
+                  <li className="text-700 py-2 text-secondary">
+                    eCommerce Store
+                  </li>
+                </ul>
+                <div className="d-flex flex-column">
+                  <a
+                    className="btn btn-lg btn-primary rounded-pill mb-3"
+                    href="#"
+                  >
+                    Start free trial
+                  </a>
+                  <a href="#">Or Start 14 days trail</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PackageSection;
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import FeaturesSection3 from "components/FeaturesSection3";
 import FeaturesSection4 from "components/FeaturesSection4";
 import HeroSection from "components/HeroSection";
 import MainLayout from "components/MainLayout";
+import PackageSection from "components/PackageSection";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,114 +20,9 @@ export default function Home() {
         <FeaturesSection2 />
         <FeaturesSection3 />
         <FeaturesSection4 />
+        <PackageSection />
 
-        {/* <section class="py-8" id="pricing">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-md-8 col-lg-6 col-xxl-5 text-center mb-3">
-              <h6 class="fw-bold fs-4 display-3 lh-sm mb-3">
-                Get awesome features, without extra charges
-              </h6>
-              <p class="mb-4">
-                The rise of mobile devices transforms the way we consume
-                information entirely and the world's most elevant channels such
-                as Facebook.
-              </p>
-            </div>
-          </div>
-          <div class="row flex-center">
-            <div class="col-12 mb-3">
-              <div class="d-flex justify-content-center">
-                <label class="form-check-label me-2" for="customSwitch1"
-                  >Monthly</label
-                >
-                <div class="form-check form-switch">
-                  <input
-                    class="form-check-input"
-                    id="customSwitch1"
-                    type="checkbox"
-                    checked="checked"
-                  />
-                  <label class="form-check-label align-top" for="customSwitch1"
-                    >Yearly</label
-                  >
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4">
-              <div class="card shadow-lg mb-4 border-0">
-                <div class="card-header border-bottom-0 pt-7 pb-5">
-                  <div class="d-flex justify-content-center">
-                    <h1 class="fw-bold">$0</h1>
-                    <span class="d-flex align-items-center">/month</span>
-                  </div>
-                  <h5 class="fw-bold text-center">Business Class</h5>
-                  <span class="text-700 text-center d-block"
-                    >For small teams or office</span
-                  >
-                </div>
-                <div class="card-body mx-auto">
-                  <ul class="list-unstyled mb-4">
-                    <li class="text-700 py-2 text-secondary">
-                      Darg &amp; Drop Builder
-                    </li>
-                    <li class="text-700 py-2 text-secondary">
-                      1,000's of Templates
-                    </li>
-                    <li class="text-700 py-2 text-secondary">
-                      Blog Support Tools
-                    </li>
-                    <li class="text-700 py-2 text-secondary">
-                      eCommerce Store
-                    </li>
-                  </ul>
-                  <a class="btn btn-lg btn-primary rounded-pill mb-3" href="#"
-                    >Start free trial</a
-                  >
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4">
-              <div class="card shadow-lg mb-4">
-                <div class="card-header border-bottom-0 pt-7 pb-5">
-                  <div class="d-flex justify-content-center">
-                    <h1 class="fw-bold">$99</h1>
-                    <span class="d-flex align-items-center">/month</span>
-                  </div>
-                  <h5 class="fw-bold text-center">Pro Master</h5>
-                  <span class="text-700 text-center d-block"
-                    >For small teams or office</span
-                  >
-                </div>
-                <div class="card-body mx-auto">
-                  <ul class="list-unstyled mb-4">
-                    <li class="text-700 py-2 text-secondary">
-                      Darg &amp; Drop Builder
-                    </li>
-                    <li class="text-700 py-2 text-secondary">
-                      1,000's of Templates
-                    </li>
-                    <li class="text-700 py-2 text-secondary">
-                      Blog Support Tools
-                    </li>
-                    <li class="text-700 py-2 text-secondary">
-                      eCommerce Store
-                    </li>
-                  </ul>
-                  <div class="d-flex flex-column">
-                    <a class="btn btn-lg btn-primary rounded-pill mb-3" href="#"
-                      >Start free trial</a
-                    ><a href="#">Or Start 14 days trail</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- end of .container-->
-      </section>
-      
-      <section class="py-8" id="testimonial">
+        {/* <section class="py-8" id="testimonial">
         <div class="container-lg">
           <div class="row flex-center">
             <div
